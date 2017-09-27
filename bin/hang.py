d8ed8a704c567777a087693403d06d7698d499bc
DesmondPrice <ortemko@list.ru>
2017-09-28 00:00:09 +0300

    ROUND

--- bin/hang.py
+++ bin/hang.py
@@ -1,50 +1,49 @@
-array = ['panda', 'joker', 'finish']
-
-
-def check_already_guessed(letter, array):
-    if letter in array:
-        return True
-    else:
-        return False
-
-
 import random
 
 
-answer = list(random.choice(array))
+array_papaprpiwsewer = ['panda', 'joker', 'finish']
+
+
+def check_already_guessed(letter, array1):
+    return letter in array1
+
+
+answer_smth = list(random.choice(array_papaprpiwsewer))
 
 
 def game():
     k = 0
-    m = 0
+    counter = 0
     riddle = []
     already_guessed = []
-    for i in range(len(answer)):
+    for i in range(len(answer_smth)):
         riddle.append('*')
-    while k<5 and m < len(answer):
+    while k < 5 and counter < len(answer_smth):
         print('Guess a letter')
         letter = input()
-        if letter in answer and not check_already_guessed(letter, already_guessed):
+        if letter in answer_smth and not check_already_guessed(letter, already_guessed):
             print('Nice!\n')
             already_guessed.append(letter)
-            for i, elem in enumerate(answer):
+            for i, elem in enumerate(answer_smth):
                 if elem == letter:
                     riddle[i] = letter
-                    m += 1
+                    counter += 1
         elif check_already_guessed(letter, already_guessed):
             print('You have already guessed this one!\n')
         else:
             k += 1
             already_guessed.append(letter)
             print('Wrong! {} mistakes out of 5\n'.format(k))
-        print('The word: ', end = '')
+        print('The word: ', end='')
         for elem in riddle:
-            print(elem, end = '')
+            print(elem, end='')
         print('\n')
 
-    if k>=5:
+    if k >= 5:
         print('Sorry! You lost!')
     else:
         print('Congratulations! You won!')
+
+
 if __name__ == '__main__':
     game()
--- tests/test.py
+++ tests/test.py
@@ -2,13 +2,14 @@ from bin.hang import check_already_guessed
 
 
 def test_check_guessed_true():
-    assert check_already_guessed('t', 'abcnotrymjkl') == True
+    assert check_already_guessed('t', 'abcnotrymjkl')
     print('OK!')
 
 
 def test_checked_guessed_false():
-    assert check_already_guessed('w', 'abcnotrymjkl') == False
+    assert not check_already_guessed('w', 'abcnotrymjkl')
     print('OK!')
+
 
 if __name__ == '__main__':
     test_check_guessed_true()
